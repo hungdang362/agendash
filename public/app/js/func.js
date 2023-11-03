edbe1ae4757a08f78eccd009e711aef17b97476a
Hung Dang <thanhhung0407@gmail.com>
2023-11-03 11:14:08 +0700

    init

---
+++ public/app/js/func.js
@@ -0,0 +1,10 @@
+function getToken() {
+  const url = location.search;
+  const query = url.substr(1);
+  const result = {};
+  query.split("&").forEach(function(part) {
+    const item = part.split("=");
+    result[item[0]] = decodeURIComponent(item[1]);
+  });
+  return result['token'] || '';
+}
